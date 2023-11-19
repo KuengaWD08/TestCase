4749436ab987c22dbc38e2069eaaafc211c778f9
KuengaWD08 <141400624+KuengaWD08@users.noreply.github.com>
2023-11-19 13:41:56 +0600

    TestCase.py

--- game/TestCase.py
+++ game/TestCase.py
@@ -80,99 +80,96 @@ class TestPlayStep(unittest.TestCase):
 
 
     def setUp(self):
-        # Set up the SnakeGame instance for testing
+       
         self.game = SnakeGame()
 
     def test_collision_with_boundary(self):
-        # Test whether _is_collision correctly detects collision with the boundary
+       
 
-        # Move the snake head outside the boundary
+        
         self.game.head = Point(-1, 0)
 
-        # Call the _is_collision method
+        
         result = self.game._is_collision()
 
-        # Check that collision is detected
+        
         self.assertTrue(result)
 
     def test_collision_with_itself(self):
-        # Test whether _is_collision correctly detects collision with itself
-
-        # Move the snake head to a position where it collides with the snake body
+       
+        
         self.game.snake = [Point(0, 0), Point(20, 0), Point(20, 20)]
         self.game.head = Point(20, 0)
 
-        # Call the _is_collision method
+        
         result = self.game._is_collision()
 
-        # Check that collision is detected
+       
         self.assertTrue(result)
 
     def test_no_collision(self):
-        # Test whether _is_collision correctly detects no collision
+       
 
-        # Move the snake head to a position where there is no collision
+        
         self.game.snake = [Point(0, 0), Point(20, 0), Point(20, 20)]
         self.game.head = Point(40, 40)
 
-        # Call the _is_collision method
+       
         result = self.game._is_collision()
 
-        # Check that no collision is detected
+        
         self.assertFalse(result)       
 
 class TestMove(unittest.TestCase):
 
     def setUp(self):
-        # Set up the SnakeGame instance for testing
+       
         self.game = SnakeGame()
 
     def test_move_right(self):
-        # Test whether _move correctly updates the position for Direction.RIGHT
+       
 
-        # Set the initial position of the snake head
+       
         self.game.head = Point(0, 0)
 
-        # Call the _move method with Direction.RIGHT
+        
         self.game._move(Direction.RIGHT)
 
-        # Check that the head is moved to the right by BLOCK_SIZE
+       
         self.assertEqual(self.game.head, Point(BLOCK_SIZE, 0))
 
     def test_move_left(self):
-        # Test whether _move correctly updates the position for Direction.LEFT
-
-        # Set the initial position of the snake head
+        
         self.game.head = Point(BLOCK_SIZE, 0)
 
-        # Call the _move method with Direction.LEFT
+        
         self.game._move(Direction.LEFT)
 
-        # Check that the head is moved to the left by BLOCK_SIZE
+        
         self.assertEqual(self.game.head, Point(0, 0))
 
     def test_move_down(self):
-        # Test whether _move correctly updates the position for Direction.DOWN
+        
 
-        # Set the initial position of the snake head
+        
         self.game.head = Point(0, 0)
 
-        # Call the _move method with Direction.DOWN
+        
         self.game._move(Direction.DOWN)
 
-        # Check that the head is moved down by BLOCK_SIZE
+       
         self.assertEqual(self.game.head, Point(0, BLOCK_SIZE))
 
     def test_move_up(self):
-        # Test whether _move correctly updates the position for Direction.UP
+        
 
-        # Set the initial position of the snake head
+
         self.game.head = Point(0, BLOCK_SIZE)
 
-        # Call the _move method with Direction.UP
+        
         self.game._move(Direction.UP)
 
-        # Check that the head is moved up by BLOCK_SIZE
+       
         self.assertEqual(self.game.head, Point(0, 0))
 
 
